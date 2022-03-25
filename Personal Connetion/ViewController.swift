b15ffdb2ee2bc645d4cc68b6f9631fe3b2d4c2a3
Grant Spraker <grant.spraker@gmail.com>
2022-03-25 11:56:54 -0600

    got it to connect to the next screen.

--- Personal Connetion/ViewController.swift
+++ Personal Connetion/ViewController.swift
@@ -40,11 +40,13 @@ import UIKit
         }
     }
 }
-class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+class ViewController: UIViewController
+{
+
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
---
+++ When_Button_is_Pressed_Controller.swift
@@ -0,0 +1,20 @@
+//
+//  When_Button_is_Pressed_Controller.swift
+//  Personal Connetion
+//
+//  Created by Grant Spraker on 3/25/22.
+//
+
+import UIKit
+
+class When_Button_is_Pressed_Controller: UIViewController
+{
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+
+    }
+    
+    
+}
